27ee852563e54972895c7b1f69cc1c484029e5b4
Ugis <ozols.ugiss@gmail.com>
2020-11-27 23:30:53 +0200

    4 nestrādā Komentārā ielikts kods kuru testēju

--- If_statement/app/src/main/java/com/example/if_statement/MainActivity.kt
+++ If_statement/app/src/main/java/com/example/if_statement/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.if_statement
 
-import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.widget.EditText
 import android.widget.Button
 import android.widget.Toast
@@ -13,50 +11,47 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
 
-    var rememberedValue= ""
-
-
     /*      Kods strādā līdz uzdevuma trešajam punktam, ceturto punktu vēl
-            Vēl īsti nesapratu kā līdz galam sataisīt to String summēšanu 
+            Vēl īsti nesapratu kā līdz galam sataisīt to String summēšanu
 
 
      */
 
-
-
+    var rememberedValue= ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val input = findViewById<EditText>(R.id.txtwText)
+        val input = findViewById<EditText>(R.id.ptInput)
         val output = findViewById<Button>(R.id.btnOutput)
         val reversedOutput = findViewById<Button>(R.id.btnReverseOutput)
 
-
-        val test1 = input.text
-        val test2 = input.text
-        rememberedValue = test1.toString()
+        var input1 = input.text.toString()
 
 
 
 
-        output.setOnClickListener() {
-        Toast.makeText(this@MainActivity, input.text, Toast.LENGTH_SHORT).show()
+       var outputFunction = output.setOnClickListener(){
+            Toast.makeText(this@MainActivity, input.text , Toast.LENGTH_SHORT).show()
+           }
+        var reversedOutputFun = reversedOutput.setOnClickListener(){
+            rememberedValue = input.text.toString().reversed()
         }
 
-        reversedOutput.setOnClickListener() {
-            if (rememberedValue == String()) {
-                val resultReversed = rememberedValue + test2.toString()
-                Toast.makeText(this@MainActivity, resultReversed.reversed(), Toast.LENGTH_SHORT).show()
-
-            } else {
-                Toast.makeText(this@MainActivity, input.text.reversed(), Toast.LENGTH_SHORT).show()
+        if(rememberedValue === input.text.toString().reversed()){
+            rememberedValue = input.text.toString() + rememberedValue
+            reversedOutput.setOnClickListener(){
+                Toast.makeText(this@MainActivity, rememberedValue, Toast.LENGTH_SHORT).show()
             }
+            }else{
+                Toast.makeText(this@MainActivity, input.text.reversed(), Toast.LENGTH_SHORT).show()
         }
-
-
     }
+}
 
-    }
+
+
+
+
 
