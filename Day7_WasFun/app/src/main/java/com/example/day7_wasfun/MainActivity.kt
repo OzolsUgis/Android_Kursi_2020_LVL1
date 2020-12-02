13bf5663728182a4ec2bd89af401ccfeb07361a5
Ugis <ozols.ugiss@gmail.com>
2020-12-02 20:51:30 +0200

    Fun works

---
+++ Day7_WasFun/app/src/main/java/com/example/day7_wasfun/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.day7_wasfun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    fun stringConc(a : String, b : String, c : String){
+        Toast.makeText(this@MainActivity, a + b + c, Toast.LENGTH_SHORT).show()
+    }
+
+    fun intComp (x : Int, y : Int){
+        if (x > y){
+            Toast.makeText(this@MainActivity, x.toString()+ " is greater than "+ y.toString(), Toast.LENGTH_SHORT).show()
+        }else {
+            Toast.makeText(this@MainActivity, y.toString()+ " is greater than "+ x.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        stringConc("My ", "Name = ", "Ugis")
+        intComp(4,6)
+    }
+}
