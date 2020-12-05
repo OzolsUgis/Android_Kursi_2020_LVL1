0636a587a9b078a9c7490125d8a6cda07af83ac4
Ugis <ozols.ugiss@gmail.com>
2020-12-05 20:27:05 +0200

    Error

--- Day9/app/src/main/java/com/example/day9/MainActivity.kt
+++ Day9/app/src/main/java/com/example/day9/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import java.util.function.BooleanSupplier
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,32 +18,88 @@ class MainActivity : AppCompatActivity() {
         val thirdBtn = findViewById<Button>(R.id.btnThird)
         val fourthBtn = findViewById<Button>(R.id.btnForth)
 
+
+
         firstBtn.setBackgroundColor(Color.RED)
         secondBtn.setBackgroundColor(Color.GREEN)
         thirdBtn.setBackgroundColor(Color.BLUE)
 
-        setListener(firstBtn)
-        setListener(secondBtn)
-        setListener(thirdBtn)
+        setListener(firstBtn,true)
+        setListener(secondBtn,true)
+        setListener(thirdBtn,true)
 
 
     }
 
-    fun setListener(btn: Button ) {
+
+
+
+
+    fun setListener(btn: Button, inputColor:Boolean) {
+
+        var color = inputColor
+        var redColor : Boolean = true
+        var greenColor : Boolean= true
+        var blueColor : Boolean=true
+
         val fourthBtn = findViewById<Button>(R.id.btnForth)
+
+
         btn.setOnClickListener {
-            if (btn.text.toString() == "1"){
-                fourthBtn.setBackgroundColor(Color.RED)
-            }else if(btn.text.toString() == "2"){
-                fourthBtn.setBackgroundColor(Color.GREEN)
-            }else if (btn.text.toString() == "3"){
-                fourthBtn.setBackgroundColor(Color.BLUE)
-            }else {
-                fourthBtn.setBackgroundColor(Color.BLACK)
+            if (btn.text.toString() == "1") {
+                if (color == false) {
+                    redColor = true
+                    color = true
+                } else if (color == true){
+                    redColor = false
+                    color = false
+                }
+
+                }
+            else if(btn.text.toString() == "2"){
+                if (color == false) {
+                    greenColor = true
+                    color == true
+                } else if (color == true){
+                    greenColor = false
+                    color= false
+                }
+
+            else if(btn.text.toString() == "3") {
+                if (color == false) {
+                    blueColor = true
+                    color = true
+                } else if (color == true) {
+                    blueColor = false
+                    color= false
+                }
+
+
+    }
+        if (redColor==true){
+            fourthBtn.setBackgroundColor(Color.RED)
+        }else if (redColor == true && greenColor == true){
+            fourthBtn.setBackgroundColor(Color.YELLOW)
+        }else if (redColor == true && blueColor==true){
+            fourthBtn.setBackgroundColor(Color.MAGENTA)
+        } else if (blueColor== true){
+            fourthBtn.setBackgroundColor(Color.BLUE)
+        }else if (blueColor== true && greenColor==true){
+            fourthBtn.setBackgroundColor(Color.CYAN)
+        } else if (greenColor==true){
+            fourthBtn.setBackgroundColor(Color.CYAN)
+        }else if (greenColor==true&&blueColor==true&&redColor==true){
+            fourthBtn.setBackgroundColor(Color.WHITE)
+
+        }
             }
         }
 
     }
+
+
+
+
 
 
 }
