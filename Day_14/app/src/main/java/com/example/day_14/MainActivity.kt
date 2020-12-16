49500a80ec65a3b5252bca8b78397750b5dac918
Ugis <ozols.ugiss@gmail.com>
2020-12-16 20:11:43 +0200

    List Added

--- Day_14/app/src/main/java/com/example/day_14/MainActivity.kt
+++ Day_14/app/src/main/java/com/example/day_14/MainActivity.kt
@@ -12,33 +12,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val num1 = findViewById<EditText>(R.id.lblNum1)
-        val num2 = findViewById<EditText>(R.id.lblNum2)
-        val num3 = findViewById<EditText>(R.id.lblNum3)
-        val num4 = findViewById<EditText>(R.id.lblNum4)
-        val num5 = findViewById<EditText>(R.id.lblNum5)
+//        val num1 = findViewById<EditText>(R.id.lblNum1)
+//        val num2 = findViewById<EditText>(R.id.lblNum2)
+//        val num3 = findViewById<EditText>(R.id.lblNum3)
+//        val num4 = findViewById<EditText>(R.id.lblNum4)
+//        val num5 = findViewById<EditText>(R.id.lblNum5)
+//
+//        val btn = findViewById<Button>(R.id.btnShowHighest)
+//        var text = findViewById<TextView>(R.id.lblOutput)
+//
+//        var numbers = IntArray(5)
+//
+//        btn.setOnClickListener(){
+//
+//            numbers[0] = num1.text.toString().toInt()
+//            numbers[1] = num2.text.toString().toInt()
+//            numbers[2] = num3.text.toString().toInt()
+//            numbers[3] = num4.text.toString().toInt()
+//            numbers[4] = num5.text.toString().toInt()
+//
+//            var highest = numbers[0]
+//
+//            for(el in numbers){
+//                if (highest < el)
+//                    highest=el
+//            }
+//
+//            text.text = highest.toString()
+//        }
 
-        val btn = findViewById<Button>(R.id.btnShowHighest)
-        var text = findViewById<TextView>(R.id.lblOutput)
 
-        var numbers = IntArray(5)
 
-        btn.setOnClickListener(){
+        val enterValue = findViewById<EditText>(R.id.lblValue)
+        val btnSaveValue = findViewById<Button>(R.id.btnSaveValue)
+        val btnOutputList= findViewById<Button>(R.id.btnOutputList)
+        val output = findViewById<TextView>(R.id.txtOutputList)
 
-            numbers[0] = num1.text.toString().toInt()
-            numbers[1] = num2.text.toString().toInt()
-            numbers[2] = num3.text.toString().toInt()
-            numbers[3] = num4.text.toString().toInt()
-            numbers[4] = num5.text.toString().toInt()
+        var values = mutableListOf<Int>()
 
-            var highest = numbers[0]
+        btnSaveValue.setOnClickListener(){
+            values.add(enterValue.text.toString().toInt())
+        }
 
-            for(el in numbers){
-                if (highest < el)
-                    highest=el
+        btnOutputList.setOnClickListener(){
+            var list :String = ""
+            for (element in values){
+                list = list + element.toString()+ ","
             }
-
-            text.text = highest.toString()
+            output.text = list
         }
     }
 }
+
